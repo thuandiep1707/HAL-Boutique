2a89496227c6132f1f9d24d978adeeb07279b006
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-05-04 10:58:39 +0800

    test

--- src/components/section/SectionContent/HomePageContent/HomePageContent.jsx
+++ src/components/section/SectionContent/HomePageContent/HomePageContent.jsx
@@ -2,27 +2,27 @@
 import './HomePageContent.scss';
 import DemoService from './DemoService';
 import { useEffect, useState } from 'react';
-// import NuocNgoai from './data/Nuocngoai.json';
-// import TrongNuoc from './data/Trongnuoc.json';
+import NuocNgoai from './data/Nuocngoai.json';
+import TrongNuoc from './data/Trongnuoc.json';
 
 
 const HomePageContent = () => {
-    // const dataNuocNgoai = NuocNgoai;
-    // const dataTrongNuoc = TrongNuoc;
-    const [dataNuocNgoai,setDataNuocNgoai] = useState([])
-    const [dataTrongNuoc,setDataTrongNuoc] = useState([])
-    useEffect(() => {
-        fetch('public/data/NuocNgoai.json')
-            .then(res => res.json())
-            .then((data) => {
-                setDataNuocNgoai(data)
-                console.log("nuocngoai")})
-        fetch('public/data/TrongNuoc.json')
-            .then(res => res.json())
-            .then((data) => {
-                setDataTrongNuoc(data)
-                console.log("trongnuoc")})    
-    },[])
+    const dataNuocNgoai = NuocNgoai;
+    const dataTrongNuoc = TrongNuoc;
+    // const [dataNuocNgoai,setDataNuocNgoai] = useState([])
+    // const [dataTrongNuoc,setDataTrongNuoc] = useState([])
+    // useEffect(() => {
+    //     fetch('public/data/NuocNgoai.json')
+    //         .then(res => res.json())
+    //         .then((data) => {
+    //             setDataNuocNgoai(data)
+    //             console.log("nuocngoai")})
+    //     fetch('public/data/TrongNuoc.json')
+    //         .then(res => res.json())
+    //         .then((data) => {
+    //             setDataTrongNuoc(data)
+    //             console.log("trongnuoc")})    
+    // },[])
     return(
         <div className="home_page_content">
             <h2 className='h3'>DU LỊCH NƯỚC NGOÀI</h2>
