61ba3c92d53ee82c9a75be5b8328b65ed043ad9b
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-05-26 17:29:16 +0700

    create loginpage

--- src/pages/authenticationpage/AuthenticationPage.jsx
+++ src/pages/authenticationpage/AuthenticationPage.jsx
@@ -1,17 +1,55 @@
 
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 import { checkLogin } from "../../services/FakeAPI";
 
 import './authenticationpage.scss'
 
 const Login = () => {
-    let res = checkLogin("user02","user02")
-    console.log(res)
+    const [loginData, setLoginData] = useState({
+        username: "",
+        password: "",
+        loading: false
+    })
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoginData({...loginData, "loading": true})
+        let check = checkLogin(loginData.username, loginData.password)
+        alert(check)
+    }
+    const handleLoginData = (type,data) => {
+        if (type == "username") setLoginData({...loginData, "username" : data})
+        if (type == "password") setLoginData({...loginData, "password" : data})
+
+    } 
+    if (loginData.loading){
+        return(
+            <div className="loading">Đang đăng nhập...</div>
+        )
+    }
     return(
-        <main className="login">
-            đăng nhập
-        </main>
+        <div className="contain-login">
+            <form className="login"onSubmit={(e)=>handleSubmit(e)}>
+                <input type="text" name="username" id="username" onChange={(e)=>handleLoginData("username",e.target.value)} required />
+                <label htmlFor="username">Username</label>
+                <input type="text" name="password" id="password" onChange={(e)=>handleLoginData("password",e.target.value)} required />
+                <label htmlFor="password">Password</label>
+                <div>
+                    <input type="checkbox" name="remember" id="remember"/>
+                    <label htmlFor="remember">Ghi nhớ đăng nhập</label>
+                </div>
+                <button>Đăng Nhập</button>
+                <div className="registerbtn">
+                    <p>Bạn chưa có tài khoản?</p> <Link to="/register">Đăng kí ngay?</Link>
+                </div>
+                <div className="login-2nd">
+                    <img src="" alt="icon" />
+                    <img src="" alt="icon" />
+                    <img src="" alt="icon" />
+                </div>
+            </form>
+        </div>
     )
 }
 
