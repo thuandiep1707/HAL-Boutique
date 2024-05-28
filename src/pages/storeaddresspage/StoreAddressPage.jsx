8ad70dc616dc8bfc68dcc3e5f0f98066aedb70f5
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-05-28 20:33:40 +0700

    refactor: update style for shop page

--- src/pages/storeaddresspage/StoreAddressPage.jsx
+++ src/pages/storeaddresspage/StoreAddressPage.jsx
@@ -4,7 +4,7 @@ import './storeaddresspage.scss'
 
 const StoreAddressPage = () => {
     return (
-        <main className='storeaddresspage'>
+        <main className='storeaddresspage' style={{height:'100vh'}}>
             <h1 style={{width:'100%', textAlign:'center', marginTop:'30px'}}>Page Not Found</h1>
         </main>
     )
