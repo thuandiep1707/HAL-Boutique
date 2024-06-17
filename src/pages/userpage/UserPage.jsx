7b253b6fcd3354839547a68afefb0e8f6ab59a75
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-06-17 11:26:45 +0700

    #10 add api get user infor

--- src/pages/userpage/UserPage.jsx
+++ src/pages/userpage/UserPage.jsx
@@ -4,29 +4,37 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 import { globalContext } from '../../context/globalContext'
 import { updateUser, getOrderList } from '../../services/controller/user.controller'
+import { getInfor } from '../../services/Auth.api'
 import './userpage.scss'
 
 const UserPage = ()=>{
     const nav = useNavigate()
     const {feature} = useParams()
-    const { userInfor, setUserInfor } = useContext(globalContext)
     const [ board, setBoard ] = useState()
-    const [orderList, setOrderList] = useState(getOrderList(userInfor?.username))
-    const [ userData, setUserData ] = useState({
-        "id": userInfor?.id,
-        "username": userInfor?.username,
-        "name": userInfor?.name,
-        "sex": userInfor?.sex,
-        "birthday": userInfor?.birthday,
-        "email": userInfor?.email,
-        "phoneNumber": userInfor?.phoneNumber,
-        "address": userInfor?.address,
-    })
+    const [ orderList, setOrderList ] = useState()    
+    const [ userID, setUserID ] = useState(sessionStorage.getItem("userID"))
+    const [ userData, setUserData ] = useState()
     useEffect(()=>{
         setBoard(feature)
         window.scrollTo({top: '0', behavior: 'smooth'})
-        if (!userInfor?.state) nav('/login')
+        if (!userID) nav('/login')
     })
+
+    useEffect(()=>{
+        setUserID(sessionStorage.getItem("userID"))
+        getUserInfor()
+    },[])
+
+    async function getUserInfor (){
+        const response = await getInfor()
+        if (response.status != 200){
+            alert(response.message)
+            nav('/')
+            return
+        }
+        setUserData(response.data)
+    }
+
     const goToPath = (url)=>nav(url)
 
     const handleChangeUserData = (key, value) => {
@@ -35,8 +43,8 @@ const UserPage = ()=>{
 
     const handleSaveInfor = (e)=>{
         e.preventDefault()
-        let response = updateUser(userData)
-        alert(response.message)
+        console.log(userData)
+        console.log(typeof userData.birthday)
     }
 
     return(
@@ -45,7 +53,7 @@ const UserPage = ()=>{
                 <div className="sidebar_avt">
                     <img src={userData?.avatar} alt="avt" className="sidebar_avt_img" />
                     <div className="sidebar_avt_username">
-                        <p>{userData.username}</p>
+                        <p>{userData?.username}</p>
                         <p onClick={()=>goToPath('/user/profile')} >Sửa hồ sơ</p>
                     </div>
                 </div>
@@ -74,27 +82,27 @@ const UserPage = ()=>{
                             <p>Hồ Sơ Của Tôi</p>
                             <p>Quản lý thông tin hồ sơ để bảo mật tài khoản</p>
                         </div>
-                        <form className="profile_form" onSubmit={(e)=>handleSaveInfor(e)}>
+                        <form className="profile_form" onSubmit={handleSaveInfor}>
                             <div className="label-list">
                                 <label htmlFor="username">Tên đăng nhập</label>
                                 <label htmlFor="name">Tên</label>
                                 <label htmlFor="email">Email</label>
                                 <label htmlFor="phone">Sô điện thoại</label>
-                                <p htmlFor="sex">Giới tính</p>
                                 <label htmlFor="birthday">Ngày sinh</label>
+                                <label htmlFor="sex">Giới tính</label>
                             </div>
                             <div className="input-list">
-                                <p>{userData.username}</p>
-                                <input type="text" name="name" id="name" defaultValue={userData.name} onChange={(e)=>handleChangeUserData('name',e.target.value)} />
-                                <input type="email" name="email" id="email" defaultValue={userData.email}/>
-                                <input type="tel" name="phone" id="phone" defaultValue={userData.phoneNumber} required pattern="[0-9]{10}" onChange={(e)=>handleChangeUserData('phoneNumber',e.target.value)} />
-                                <input type="date" name="birthday" id="birthday" defaultValue={userData.birthday} />
+                                <p>{userData?.username}</p>
+                                <input type="text" name="name" id="name" defaultValue={userData?.name} onChange={(e)=>handleChangeUserData('name',e.target.value)} />
+                                <input type="email" name="email" id="email" defaultValue={userData?.email}/>
+                                <input type="tel" name="phone" id="phone" defaultValue={userData?.phoneNumber} required pattern="[0-9]{10}" onChange={(e)=>handleChangeUserData('phoneNumber',e.target.value)} />
+                                <input type="date" name="birthday" id="birthday" defaultValue={userData?.birthday} onChange={(e)=>handleChangeUserData('birthday',e.target.value)} />
                                 <div className="sex-options">
-                                    <input type="radio" name="sex" id="male" checked={userData.sex == "male" ? true : false}  onChange={()=>handleChangeUserData('sex', 'male')}/>
+                                    <input type="radio" name="sex" id="male" checked={userData?.sex == "male" ? true : false}  onChange={()=>handleChangeUserData('sex', 'male')}/>
                                     <label htmlFor="male"> Nam </label>
-                                    <input type="radio" name="sex" id="female" checked={userData.sex == "female" ? true : false} onChange={()=>handleChangeUserData('sex', 'female')} />
+                                    <input type="radio" name="sex" id="female" checked={userData?.sex == "female" ? true : false} onChange={()=>handleChangeUserData('sex', 'female')} />
                                     <label htmlFor="female"> Nữ </label>
-                                    <input type="radio" name="sex" id="null" checked={userData.sex == null ? true : false} onChange={()=>handleChangeUserData('sex', null)} />
+                                    <input type="radio" name="sex" id="null" checked={userData?.sex == null ? true : false} onChange={()=>handleChangeUserData('sex', null)} />
                                     <label htmlFor="null"> Khác </label>
                                 </div>
                                 <button className="infor_list_btn pointer">Lưu</button>
