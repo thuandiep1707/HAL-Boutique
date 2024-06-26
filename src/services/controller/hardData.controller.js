c090dfe38531030bcb5ac3dbdcd1bd6c0fae5383
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-06-26 12:36:49 +0700

    delete old client

--- src/services/controller/hardData.controller.js
+++
@@ -1,7 +0,0 @@
-import halProjectDB from "../model/database.model";
-
-const homeSlides = halProjectDB.slideData
-const promotionList = halProjectDB.promotionList
-const hotnewsList = halProjectDB.hotnewsList
-
-export { homeSlides, promotionList, hotnewsList }
